4a3e9f413760f6800f555bd7e945c871b9c4ed32
Leury Wilmer Osorno <leury.osorno@correo.tdea.edu.co>
2023-06-02 23:01:43 -0500

    Inicio de app para taller final

---
+++ componentes/MainNavigation.js
@@ -0,0 +1,50 @@
+import * as React from 'react'
+import {createStackNavigator} from '@react-navigation/stack'
+import Home from '../screens/Home';
+import Navbar from './Navbar';
+import Detail from '../screens/Detail';
+import Search from '../screens/Search';
+const Stack = createStackNavigator()
+
+class MainNavigation extends React.PureComponent{
+    render(){
+        return(
+            <Stack.Navigator headerMode={'screen'}>
+                <Stack.Screen
+                name = "Home"
+                component={Home}
+                options={{
+                    headerTransparent:true,
+                    header:({navigation}) => (
+                        <Navbar navigation={navigation} main={true} />  
+                    ),
+                }}
+                />
+              <Stack.Screen
+                    name="Detail"
+                    component={Detail}
+                    options={{
+                    headerTransparent: true,
+                    header: ({navigation}) => (
+                        <Navbar main={false} navigation={navigation} />
+                    ),
+                    }}
+                />
+                <Stack.Screen
+                name="Search"
+                component={Search}
+                options={{
+                    headerTransparent: true,
+                    header: ({navigation}) => (
+                    <Navbar main={false} navigation={navigation} />
+                    ),
+                }}
+                /> 
+
+            </Stack.Navigator>
+            
+        )
+    }
+}
+
+export default MainNavigation;
